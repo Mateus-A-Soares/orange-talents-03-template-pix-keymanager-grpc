635b49d900985fced0054c294097834e687f1b6c
Mateus-A-Soares <soares.almeida.mateus@gmail.com>
2021-05-28 16:00:03 -0300

    Testes relacionados ao registro da chave pix atualizados

--- src/test/kotlin/br/com/zupacademy/chave/cadastro/RegistraChavePixTest.kt
+++ src/test/kotlin/br/com/zupacademy/chave/cadastro/RegistraChavePixTest.kt
@@ -2,11 +2,12 @@ package br.com.zupacademy.chave.cadastro
 
 import br.com.zupacademy.*
 import br.com.zupacademy.CadastraChavePixServiceGrpc.CadastraChavePixServiceBlockingStub
-import br.com.zupacademy.bcb.BcbClient
+import br.com.zupacademy.bcb.*
 import br.com.zupacademy.chave.ChavePix
 import br.com.zupacademy.chave.ChavePixRespository
 import br.com.zupacademy.chave.TipoChave
 import br.com.zupacademy.chave.TipoConta
+import br.com.zupacademy.chave.conta.ItauBank
 import br.com.zupacademy.itauerp.BuscarContaTipoItauErpResponse
 import br.com.zupacademy.itauerp.InstituicaoResponse
 import br.com.zupacademy.itauerp.ItauErpClient
@@ -19,6 +20,7 @@ import io.micronaut.context.annotation.Factory
 import io.micronaut.grpc.annotation.GrpcChannel
 import io.micronaut.grpc.server.GrpcServerChannel
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.test.annotation.MockBean
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -26,10 +28,15 @@ import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.mockito.InjectMocks
 import org.mockito.Mockito
 import org.mockito.Mockito.`when`
+import java.time.LocalDateTime
 import java.util.*
+import java.util.stream.Stream
 import javax.inject.Inject
 
 @MicronautTest(transactional = false)
@@ -38,12 +45,23 @@ internal class RegistraChavePixTest(
     @InjectMocks val grpcClient: CadastraChavePixServiceBlockingStub
 ) {
 
-    companion object {
+    private companion object {
         val CLIENTE_ID = UUID.randomUUID()
+
+        @JvmStatic
+        fun chavePixValida() = Stream.of(
+            Arguments.of(TipoChaveRequest.TIPO_CHAVE_CPF, "11111111111"),
+            Arguments.of(TipoChaveRequest.TIPO_CHAVE_EMAIL, "teste@email.com"),
+            Arguments.of(TipoChaveRequest.TIPO_CHAVE_TELEFONE, "+551111111111"),
+            Arguments.of(TipoChaveRequest.TIPO_CHAVE_ALEATORIA, "")
+        )
     }
 
     @Inject
     lateinit var itauErpClient: ItauErpClient
+
+    @Inject
+    lateinit var bcbClient: BcbClient
 
     @MockBean(ItauErpClient::class)
     fun itauClient(): ItauErpClient {
@@ -68,23 +86,34 @@ internal class RegistraChavePixTest(
         repository.deleteAll()
     }
 
-    @Test
-    fun deveRegistrarChave() {
+    @ParameterizedTest
+    @MethodSource("chavePixValida")
+    fun `deve registrar chave`(tipoChaveRequest: TipoChaveRequest, valorChave: String) {
+        val request = CadastraChavePixRequest.newBuilder()
+            .setClienteId(CLIENTE_ID.toString())
+            .setTipoChave(tipoChaveRequest)
+            .setValorChave(valorChave)
+            .setTipoConta(TipoContaRequest.TIPO_CONTA_CORRENTE)
+            .build()
+        val itauClientResponse = itauClientResponse()
         `when`(
             itauErpClient.buscaPorContaTipo(
-                clienteId = CLIENTE_ID.toString(),
+                clienteId = request.clienteId,
                 tipoConta = TipoConta.CORRENTE.itauErpParameterName
             )
-        )
-            .thenReturn(itauClientResponse())
-        val response: ChavePixCadastradaResponse = grpcClient.registra(
-            CadastraChavePixRequest.newBuilder()
-                .setClienteId(CLIENTE_ID.toString())
-                .setTipoChave(TipoChaveRequest.TIPO_CHAVE_EMAIL)
-                .setValorChave("teste@email.com")
-                .setTipoConta(TipoContaRequest.TIPO_CONTA_CORRENTE)
-                .build()
-        )
+        ).thenReturn(itauClientResponse)
+        `when`(
+            bcbClient.cadastraChave(
+                request = CadastraChavePixBcbRequest(
+                    keyType = KeyTypeBcb.of(request.tipoChave.toModel()!!),
+                    key = request.valorChave,
+                    bankAccount = BankAccountBcbRequest.of(itauClientResponse!!.body().toModel()),
+                    owner = OwnerBcbRequest.of(itauClientResponse!!.body().titular),
+                )
+            )
+        ).thenReturn(bcbClientResponse(request, itauClientResponse.body()))
+
+        val response: ChavePixCadastradaResponse = grpcClient.registra(request)
 
         with(response) {
             assertNotNull(chavePixId)
@@ -92,53 +121,41 @@ internal class RegistraChavePixTest(
     }
 
     @Test
-    fun naoDeveRegistrarChaveComErrosDeValidacao() {
-        `when`(
-            itauErpClient.buscaPorContaTipo(
-                clienteId = CLIENTE_ID.toString(),
-                tipoConta = TipoConta.CORRENTE.itauErpParameterName
-            )
-        )
-            .thenReturn(itauClientResponse())
+    fun `nao deve registrar chave com erros de validacao`() {
+        val request = CadastraChavePixRequest.newBuilder()
+            .setClienteId("")
+            .setTipoConta(TipoContaRequest.TIPO_CONTA_CORRENTE)
+            .setTipoChave(TipoChaveRequest.TIPO_CHAVE_CPF)
+            .setValorChave("teste@email.com")
+            .build()
 
         val exception = assertThrows<StatusRuntimeException> {
-            grpcClient.registra(
-                CadastraChavePixRequest.newBuilder()
-                    .setClienteId("")
-                    .setTipoConta(TipoContaRequest.TIPO_CONTA_CORRENTE)
-                    .setTipoChave(TipoChaveRequest.TIPO_CHAVE_CPF)
-                    .setValorChave("teste@email.com")
-                    .build()
-            )
+            grpcClient.registra(request)
         }
-
 
         with(exception) {
             assertEquals(Status.INVALID_ARGUMENT.code, status.code)
             assertEquals("Dados inválidos", status.description)
-//            exception.
         }
     }
 
     @Test
-    fun naoDeveCadastrarChaveComContaInexistente() {
+    fun `nao deve cadastrar chave com conta inexistente`() {
+        val request = CadastraChavePixRequest.newBuilder()
+            .setClienteId(CLIENTE_ID.toString())
+            .setTipoChave(TipoChaveRequest.TIPO_CHAVE_EMAIL)
+            .setValorChave("teste@email.com")
+            .setTipoConta(TipoContaRequest.TIPO_CONTA_CORRENTE)
+            .build()
         `when`(
             itauErpClient.buscaPorContaTipo(
-                clienteId = CLIENTE_ID.toString(),
+                clienteId = request.clienteId,
                 tipoConta = TipoConta.CORRENTE.itauErpParameterName
             )
-        )
-            .thenReturn(HttpResponse.notFound())
+        ).thenReturn(HttpResponse.notFound())
 
         val exception = assertThrows<StatusRuntimeException> {
-            grpcClient.registra(
-                CadastraChavePixRequest.newBuilder()
-                    .setClienteId(CLIENTE_ID.toString())
-                    .setTipoChave(TipoChaveRequest.TIPO_CHAVE_EMAIL)
-                    .setValorChave("teste@email.com")
-                    .setTipoConta(TipoContaRequest.TIPO_CONTA_CORRENTE)
-                    .build()
-            )
+            grpcClient.registra(request)
         }
 
         with(exception) {
@@ -148,33 +165,41 @@ internal class RegistraChavePixTest(
     }
 
     @Test
-    fun naoDeveCadastrarChaveComChaveExistente() {
+    fun `nao deve cadastrar chave ja cadastrada no banco`() {
+        val itauResponse = itauClientResponse()
+        val chavePix = repository.save(
+            ChavePix(
+                tipoChave = TipoChave.EMAIL,
+                chave = "teste@email.com",
+                clienteId = CLIENTE_ID,
+                conta = itauResponse!!.body().toModel()
+            )
+        )
+        val request = CadastraChavePixRequest.newBuilder()
+            .setClienteId(chavePix.clienteId.toString())
+            .setTipoChave(TipoChaveRequest.TIPO_CHAVE_EMAIL)
+            .setValorChave(chavePix.chave)
+            .setTipoConta(TipoContaRequest.TIPO_CONTA_CORRENTE)
+            .build()
         `when`(
             itauErpClient.buscaPorContaTipo(
                 clienteId = CLIENTE_ID.toString(),
                 tipoConta = TipoConta.CORRENTE.itauErpParameterName
             )
-        )
-            .thenReturn(itauClientResponse())
-
-        repository.save(
-            ChavePix(
-                tipoChave = TipoChave.EMAIL,
-                chave = "teste@email.com",
-                clienteId = CLIENTE_ID,
-                conta = itauClientResponse()!!.body().toModel()
+        ).thenReturn(itauResponse)
+        `when`(
+            bcbClient.cadastraChave(
+                request = CadastraChavePixBcbRequest(
+                    keyType = KeyTypeBcb.of(request.tipoChave.toModel()!!),
+                    key = request.valorChave,
+                    bankAccount = BankAccountBcbRequest.of(itauResponse!!.body().toModel()),
+                    owner = OwnerBcbRequest.of(itauResponse!!.body().titular),
+                )
             )
-        )
+        ).thenReturn(bcbClientResponse(request, itauResponse.body()))
 
         val exception = assertThrows<StatusRuntimeException> {
-            grpcClient.registra(
-                CadastraChavePixRequest.newBuilder()
-                    .setClienteId(CLIENTE_ID.toString())
-                    .setTipoChave(TipoChaveRequest.TIPO_CHAVE_EMAIL)
-                    .setValorChave("teste@email.com")
-                    .setTipoConta(TipoContaRequest.TIPO_CONTA_CORRENTE)
-                    .build()
-            )
+            grpcClient.registra(request)
         }
 
         with(exception) {
@@ -183,14 +208,87 @@ internal class RegistraChavePixTest(
         }
     }
 
-    private fun itauClientResponse(): HttpResponse<BuscarContaTipoItauErpResponse>? {
+    @Test
+    fun `nao deve cadastrar chave ja cadastrada no servico bcb`() {
+        val request = CadastraChavePixRequest.newBuilder()
+            .setClienteId(CLIENTE_ID.toString())
+            .setTipoChave(TipoChaveRequest.TIPO_CHAVE_EMAIL)
+            .setValorChave("chavecadastrada@bcb")
+            .setTipoConta(TipoContaRequest.TIPO_CONTA_CORRENTE)
+            .build()
+        val itauResponse = itauClientResponse()
+        val bcbResponse = HttpResponse.unprocessableEntity<Problem>().body(
+            Problem(
+                type = "UNPROCESSABLE_ENTITY",
+                status = 422,
+                title = "Unprocessable Entity",
+                detail = "The informed Pix key exists already",
+                violations = null
+            )
+        )
+        `when`(
+            itauErpClient.buscaPorContaTipo(
+                clienteId = CLIENTE_ID.toString(),
+                tipoConta = TipoConta.CORRENTE.itauErpParameterName
+            )
+        ).thenReturn(itauResponse)
+        `when`(
+            bcbClient.cadastraChave(
+                request = CadastraChavePixBcbRequest(
+                    keyType = KeyTypeBcb.of(request.tipoChave.toModel()!!),
+                    key = request.valorChave,
+                    bankAccount = BankAccountBcbRequest.of(itauResponse!!.body().toModel()),
+                    owner = OwnerBcbRequest.of(itauResponse!!.body().titular),
+                )
+            )
+        ).thenThrow(HttpClientResponseException("", bcbResponse))
+
+        val exception = assertThrows<StatusRuntimeException> {
+            grpcClient.registra(request)
+        }
+
+        with(exception) {
+            assertEquals(Status.ALREADY_EXISTS.code, status.code)
+            assertEquals(bcbResponse.body.get().detail, status.description)
+        }
+    }
+
+    private fun itauClientResponse(): HttpResponse<BuscarContaTipoItauErpResponse> {
         return HttpResponse.ok(
             BuscarContaTipoItauErpResponse(
                 tipo = TipoConta.CORRENTE.itauErpParameterName,
                 agencia = "",
                 numero = "",
-                instituicao = InstituicaoResponse(nome = "", ispb = ""),
+                instituicao = InstituicaoResponse(nome = "", ispb = ItauBank.ISPB),
                 titular = TitularResponse(id = CLIENTE_ID.toString(), nome = "", cpf = "")
+            )
+        )
+    }
+
+    private fun bcbClientResponse(
+        chaveRequest: CadastraChavePixRequest,
+        itauErpResponse: BuscarContaTipoItauErpResponse
+    ): HttpResponse<ChavePixBcbDetailsResponse> {
+        val conta = itauErpResponse.toModel()
+        val titular = itauErpResponse.titular
+        val keyType = KeyTypeBcb.of(chaveRequest.tipoChave.toModel()!!)
+        val key = if (keyType != KeyTypeBcb.RANDOM) chaveRequest.valorChave else UUID.randomUUID().toString()
+        return HttpResponse.ok(
+            ChavePixBcbDetailsResponse(
+                keyType = keyType,
+                key = key,
+                bankAccount = BankAccountBcbResponse(
+                    accountNumber = conta.numero,
+                    accountType = AccountTypeBcb.of(conta.tipo),
+                    branch = conta.agencia,
+                    participant = ItauBank.ISPB
+                ),
+                owner = OwnerBcbResponse(
+                    type = TypeBcb.NATURAL_PERSON,
+                    name = titular.nome,
+                    taxIdNumber = titular.cpf
+                ),
+                createdAt = LocalDateTime.now()
             )
         )
     }
